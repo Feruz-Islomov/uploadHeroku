9bdf40ba5e77e38874b0946364233341b02d8ce1
Abdulloh <firuzwest@gmail.com>
2022-04-29 15:52:45 +0500

    delete feature

--- serverproject/database.js
+++ serverproject/database.js
@@ -1,11 +1,13 @@
-'use strict';
-const mongoose = require('mongoose');
+"use strict";
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/upload-files-database', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: true,
-    }).then(() => console.log('Connected to Mongodb......'));
-}
+  mongoose
+    .connect("mongodb://localhost/upload-files-database", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("Connected to Mongodb......"));
+};
--- serverproject/routes/file-upload-routes.js
+++ serverproject/routes/file-upload-routes.js
@@ -1,18 +1,24 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const {upload} = require('../helpers/filehelper');
-const {singleFileUpload, multipleFileUpload,
-     getallSingleFiles, getallMultipleFiles} = require('../controllers/fileuploaderController');
+const express = require("express");
+const { upload } = require("../helpers/filehelper");
+const {
+  singleFileUpload,
+  multipleFileUpload,
+  getallSingleFiles,
+  getallMultipleFiles,
+  singleFileDelete,
+  multipleFileDelete,
+} = require("../controllers/fileuploaderController");
 const router = express.Router();
 
-
-router.post('/singleFile', upload.single('file'), singleFileUpload);
-router.post('/multipleFiles', upload.array('files'), multipleFileUpload);
-router.get('/getSingleFiles', getallSingleFiles);
-router.get('/getMultipleFiles', getallMultipleFiles);
-
+router.post("/singleFile", upload.single("file"), singleFileUpload);
+router.post("/multipleFiles", upload.array("files"), multipleFileUpload);
+router.get("/getSingleFiles", getallSingleFiles);
+router.get("/getMultipleFiles", getallMultipleFiles);
+router.delete("/singleFileDelete", singleFileDelete);
+router.delete("/multipleFileDelete", multipleFileDelete);
 
 module.exports = {
-    routes: router
-}
+  routes: router,
+};
